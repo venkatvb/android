3093aebcf6fae17082caf6e3619b0d563efc5701
venkatvb <venkateshbabu95@gmail.com>
2014-06-26 22:40:34 +0530

    Handle loader and handle validatation commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 var app = angular.module('myApp', [
         "mobile-angular-ui"
     ]);
-app.controller("myController", function($scope){
+app.controller("myController", function($scope, $http, $timeout){
 
     $scope.user = {
         "handle": ""
     };
-
+    $scope.$watch("user.handle", function() 
+    {
+        $scope.user.handle = $scope.user.handle.toLowerCase().replace(/\s+/g,'');
+    });
     $scope.usr = [];
     refreshHandles();
+    $scope.handleloading = false;
     $scope.add = function()
     {
+        $scope.handleloading = true;
        // console.log(localStorage.appinit);
         if(localStorage.appinit == null)
         {
@@ -20,15 +25,33 @@ app.controller("myController", function($scope){
         {
             localStorage.setItem("cfid", 0);
         }
-        var storedNames = JSON.parse(localStorage['cfhandle']);
-        var index = parseInt(localStorage.getItem("cfid"));
-        storedNames[index] = JSON.stringify(new handl($scope.user.handle));
-        index = index + 1;
-        localStorage.setItem("cfid", index);
-        localStorage.setItem("cfhandle", JSON.stringify(storedNames));
-       // console.log(localStorage.cfhandle);
-        $scope.user.handle = "";
-        refreshHandles();
+        console.log("the scope.user.handle is "+$scope.user.handle);
+        var url = "http://json2jsonp.com/?url=http://codeforces.com/api/user.info?handles="+$scope.user.handle+"&callback=cfhandle";
+        $http.jsonp(url);
+        window.cfhandle = function(data)
+        {
+            $scope.handleloading = false;
+            console.log("cfhandle called");
+            $scope.state = data.status;
+            console.log("The status of the data is "+data.status);
+            if(data.status == "FAILED")
+            {
+                console.log("the handle doesn't exists");
+            }
+            if(data.status == "OK")
+            {
+                console.log("do the local storage here..");
+                var storedNames = JSON.parse(localStorage['cfhandle']);
+                var index = parseInt(localStorage.getItem("cfid"));
+                storedNames[index] = JSON.stringify(new handl($scope.user.handle));
+                index = index + 1;
+                localStorage.setItem("cfid", index);
+                localStorage.setItem("cfhandle", JSON.stringify(storedNames));
+               // console.log(localStorage.cfhandle);
+                $scope.user.handle = "";
+                refreshHandles();
+            }
+        }
     }
     function init()
     {
